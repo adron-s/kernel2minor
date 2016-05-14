feba503c5a436a5e3ba40f003f4c4fa245bbc688
Sergey Sergeev <adron@yapic.net>
2016-05-15 01:10:52 +0300

    ar71xx: tools/kernel2minor. Remove kernel2mikrotikyaffs2 section from Target Images submenu.
    It is no longer necessary because now to get mikrotik’s yaffs2 kernel image we using
    KERNEL := … | kernel2mikor … in target/linux/ar71xx/image/Makefile -> Device/rb-… section.
    
    Signed-off-by: Sergey Sergeev <adron@yapic.net>

--- pack.sh
+++ pack.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-KERNEL_NOR="/home/prog/openwrt/trunk-rb941-2nd/bin/ar71xx/openwrt-ar71xx-mikrotik-vmlinux-lzma.elf"
-KERNEL_NAND="/home/prog/openwrt/trunk/bin/ar71xx/openwrt-ar71xx-mikrotik-vmlinux-lzma.elf"
+KERNEL="/home/prog/openwrt/trunk-all/bin/ar71xx/openwrt-ar71xx-mikrotik-vmlinux-lzma.elf"
+KERNEL="/home/prog/openwrt/trunk-rb941-2nd/bin/ar71xx/openwrt-ar71xx-mikrotik-vmlinux-lzma.elf"
 
-RESNAME_NOR=$(basename $KERNEL_NOR | sed -e 's/\..\+//')
-RESNAME_NAND=$(basename $KERNEL_NAND | sed -e 's/\..\+//')
+RESNAME=$(basename $KERNEL | sed -e 's/\..\+//')
 
 #for NOR
-./kernel2minor -k $KERNEL_NOR -r ./$RESNAME_NOR.nor-tik-yaffs2
- -s 1024 -e
+./kernel2minor -k $KERNEL -r ./$RESNAME.nor-tik-yaffs2.bin -s 1024 -e
 #for NAND
-./kernel2minor -k $KERNEL_NAND -r ./$RESNAME_NAND.nand-tik-yaffs2-2048b-ecc -s 2048 -c -e
+#./kernel2minor -k $KERNEL -r ./$RESNAME.nand-tik-yaffs2-2048b-ecc.bin -s 2048 -c -e
